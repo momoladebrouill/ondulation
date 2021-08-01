160f65d80c4b0ad748074a40ea935dedc02321ec
momoladebrouill <momoladebrouill@gmail.com>
2021-08-01 17:42:56 +0200

    Update onde sur fil - Copie.py
    enlever le useless bvullshit

--- onde sur fil - Copie.py
+++ onde sur fil - Copie.py
@@ -11,14 +11,15 @@ pg.init()
 f = pg.display.set_mode(size=(WIND, WIND))
 pg.display.set_caption("")
 fpsClock = pg.time.Clock()
-font = pg.font.Font('consolas.ttf', 30) #police//roxane
+font = pg.font.SysFont('consolas', 15) #police//roxane
 quis=0
 b = True
 def draw(x,y):
     pg.draw.rect(f,(255,255,255),pg.Rect((x*WIND,WIND/2-y*WIND/2),(10,10)))
-coef=1
+coef=2
 L=[0 for i in range(int(WIND/10))]
 lin=len(L)
+userset=False
 def src(L,ind):
     if ind<0:
         return math.cos(quis)
@@ -41,19 +42,13 @@ try:
         textRect = text.get_rect()
 
         p = pg.key.get_pressed()  # SI la touche est appuyée
-        """if p[pg.K_d]:depx-=mouv
-        if p[pg.K_q]:depx+=mouv
-        if p[pg.K_z]:depy+=mouv
-        if p[pg.K_s]:depy-=mouv"""
+    
 
-        
-        
-        
-        pos=pg.mouse.get_pos()
         for i in range(lin):
             L[i]=(src(L,i-1)*coef+src(L,i+1))/(1+coef)
         for ind,elem in enumerate(L):
             draw(ind/lin,elem)
+        del ind,elem
         quis+=1/32
         for event in pg.event.get():  # QUAND la touche est appuyée
             if event.type == pg.QUIT:
@@ -65,21 +60,28 @@ try:
                 if event.dict['key']==pg.K_a:"""
                     
             elif event.type==pg.MOUSEBUTTONUP:
-                """if event.button==1: #click gauche
-                    pos=event.pos
-
-
+                if event.button==1: #click gauche
+                    userset=False
+                    
                     
 
-                if event.button==3: #click droit
+                """if event.button==3: #click droit
                    """
                 if event.button==4: #vers le haut
                     coef+=0.01
                 elif event.button==5: #vers le bas
                     coef-=0.01
-
+            elif event.type==pg.MOUSEBUTTONDOWN:
+                if event.button==1: #click gauche
+                    userset=True
         
-        
+        if userset:
+            quis=pg.mouse.get_pos()[1]/75
+            decal=0
+            for i in dir():
+                g=font.render(i+" = "+str(eval(i)),1,(255,255,255))
+                decal+=g.get_rect().height
+                f.blit(g,(0,decal))
         f.blit(text, (0,0))
 
 
